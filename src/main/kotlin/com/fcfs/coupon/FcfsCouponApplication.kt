2fc25e2e3d222adffec3496684d0a25746cdd348
Picbel <picbel.gon@gmail.com>
2023-04-19 01:07:43 +0900

    기획 사항 정의

---
+++ src/main/kotlin/com/fcfs/coupon/FcfsCouponApplication.kt
@@ -0,0 +1,11 @@
+package com.fcfs.coupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FcfsCouponApplication
+
+fun main(args: Array<String>) {
+    runApplication<FcfsCouponApplication>(*args)
+}
---
+++ src/test/kotlin/com/fcfs/coupon/FcfsCouponApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fcfs.coupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcfsCouponApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

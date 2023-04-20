c5c4e4854f5cbb133669e4d16229ac4402501aa9
Picbel <picbel.gon@gmail.com>
2023-04-21 00:49:33 +0900

    [Spec]: FirstComeCouponEvent 테스트 코드 작성
    [변경 사항]:FirstComeCouponEvent 테스트 코드 작성
    - countConsecutiveCouponDays()
     - 유저가 몇일 연속 쿠폰을 발급하였는지 확인합니다.
    - isConsecutiveCouponEligible()
     - 연속 쿠폰 발급이 가능한지 확인합니다.
    
    [참고 사항]: 없습니다

--- src/main/kotlin/com/fcfs/coupon/core/domain/firstcome/FirstComeCouponEvent.kt
+++ src/main/kotlin/com/fcfs/coupon/core/domain/firstcome/FirstComeCouponEvent.kt
@@ -13,7 +13,31 @@ data class FirstComeCouponEvent(
     val description: String,
     val limitCount: Long,
     val specialLimitCount: Long,
-    val history: List<FirstComeCouponEventHistory>,
+    val history: MutableList<FirstComeCouponEventHistory>,
     val startDate: LocalDate,
     val endDate: LocalDate,
-)
+) {
+    /**
+     * 쿠폰 발급 이력을 기록합니다.
+     */
+    fun recordSupplyCouponHistory(
+        userId: Long,
+        couponId: Long,
+    ): Boolean {
+        TODO()
+    }
+
+    /**
+     * 몇유저가 몇일 연속 쿠폰을 발급하였는지 확인합니다.
+     */
+    fun countConsecutiveCouponDays(): Long {
+        TODO()
+    }
+
+    /**
+     * 연속 쿠폰 발급이 가능한지 확인합니다.
+     */
+    fun isConsecutiveCouponEligible(): Boolean {
+        TODO()
+    }
+}
--- src/test/kotlin/com/fcfs/coupon/testcase/small/core/domain/firstcome/FirstComeCouponEventSpec.kt
+++ src/test/kotlin/com/fcfs/coupon/testcase/small/core/domain/firstcome/FirstComeCouponEventSpec.kt
@@ -1,5 +1,91 @@
 package com.fcfs.coupon.testcase.small.core.domain.firstcome
 
+import com.fcfs.coupon.core.domain.firstcome.FirstComeCouponEvent
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+@Disabled
+@Suppress("NonAsciiCharacters") // 테스트 코드의 가독성을 위해 함수명에 한글을 사용합니다.
 class FirstComeCouponEventSpec {
 
+    private val testUserId = 1L
+
+    @Test
+    fun `유저의 이벤트 연속 응모 횟수를 조회합니다`() {
+        //given
+        val sut: FirstComeCouponEvent = setUpFirstComeCouponEvent(createDates = 10)
+        //when
+        val count = sut.countConsecutiveCouponDays()
+        //then
+        count shouldBe 10
+    }
+
+    @Test
+    fun `3일 연속 선착순 유저는 연속 쿠폰 대상자입니다`() {
+        //given
+        val sut: FirstComeCouponEvent = setUpFirstComeCouponEvent(createDates = 3)
+        //when
+        val expect = sut.isConsecutiveCouponEligible()
+        //then
+        expect shouldBe true
+    }
+
+    @Test
+    fun `5일 연속 선착순 유저는 연속 쿠폰 대상자입니다`() {
+        //given
+        val sut: FirstComeCouponEvent = setUpFirstComeCouponEvent(createDates = 5)
+        //when
+        val expect = sut.isConsecutiveCouponEligible()
+        //then
+        expect shouldBe true
+    }
+
+    @Test
+    fun `7일 연속 선착순 유저는 연속 쿠폰 대상자입니다`() {
+        //given
+        val sut: FirstComeCouponEvent = setUpFirstComeCouponEvent(createDates = 7)
+        //when
+        val expect = sut.isConsecutiveCouponEligible()
+        //then
+        expect shouldBe true
+    }
+
+    @Test
+    fun `6일 연속 선착순 유저는 연속 쿠폰 대상자가 아닙니다`() {
+        //given
+        val sut: FirstComeCouponEvent = setUpFirstComeCouponEvent(createDates = 7)
+        //when
+        val expect = sut.isConsecutiveCouponEligible()
+        //then
+        expect shouldBe false
+    }
+
+    @Test
+    fun `8일 연속 선착순 유저는 연속 쿠폰 대상자가 아닙니다`() {
+        //given
+        val sut: FirstComeCouponEvent = setUpFirstComeCouponEvent(createDates = 7)
+        //when
+        val expect = sut.isConsecutiveCouponEligible()
+        //then
+        expect shouldBe false
+    }
+
+    @Test
+    fun `10일 연속 선착순 유저는 연속 쿠폰 대상자입니다`() {
+        //given
+        val sut: FirstComeCouponEvent = setUpFirstComeCouponEvent(createDates = 7)
+        //when
+        val expect = sut.isConsecutiveCouponEligible()
+        //then 7일차에 연속카운트 reset이후 3일이 되는 시점이라 연속 쿠폰 대상자입니다.
+        expect shouldBe false
+    }
+
+    /**
+     * createDates 만큼 진행된 FirstComeCouponEvent를 생성합니다.
+     */
+    private fun setUpFirstComeCouponEvent(createDates: Int): FirstComeCouponEvent {
+        TODO()
+    }
 }

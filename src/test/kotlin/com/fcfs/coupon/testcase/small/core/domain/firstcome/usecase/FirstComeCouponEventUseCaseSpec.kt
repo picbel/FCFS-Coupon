12cac0ef8cc7a82e47dc4679561065da997fa8c3
Picbel <picbel.gon@gmail.com>
2023-04-24 16:02:08 +0900

    [spec]: FirstComeCouponEventUseCase의 usecase스펙 정의
    [변경 사항]:
    - FirstComeCouponEventUseCase의 applyForFirstComeCouponEvent 요구사항을 정의하였습니다

---
+++ src/test/kotlin/com/fcfs/coupon/testcase/small/core/domain/firstcome/usecase/FirstComeCouponEventUseCaseSpec.kt
@@ -0,0 +1,30 @@
+package com.fcfs.coupon.testcase.small.core.domain.firstcome.usecase
+
+
+@Suppress("NonAsciiCharacters", "ClassName") // 테스트 코드의 가독성을 위해 함수명과 클레스에 한글을 사용합니다.
+class FirstComeCouponEventUseCaseSpec {
+
+    fun `선착순 유저입니다`(){
+        // given
+
+        // when
+
+        // then 쿠폰 발급여부 확인
+    }
+
+    fun `3일 연속 선착순 유저입니다`(){
+        // given
+
+        // when
+
+        // then 연속 쿠폰 발급여부 확인
+    }
+
+    fun `유저가 선착순에 들지 못했습니다`(){
+        // given
+
+        // when
+
+        // then 실패결과 확인
+    }
+}

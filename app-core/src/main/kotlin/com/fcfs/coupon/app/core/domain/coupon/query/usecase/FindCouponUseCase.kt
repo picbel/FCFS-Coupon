8d563756706e74d39ce942950d328ad15333adc1
Picbel <picbel.gon@gmail.com>
2024-04-09 00:14:09 +0900

    [feat] FindCouponUseCase 정의

--- app-core/src/main/kotlin/com/fcfs/coupon/app/core/domain/coupon/query/usecase/FindCouponUseCase.kt
+++ app-core/src/main/kotlin/com/fcfs/coupon/app/core/domain/coupon/query/usecase/FindCouponUseCase.kt
@@ -1,4 +1,13 @@
 package com.fcfs.coupon.app.core.domain.coupon.query.usecase
 
-class FindCouponUseCase {
+import com.fcfs.coupon.app.core.domain.coupon.query.readmodel.IssuedCoupon
+import com.fcfs.coupon.app.core.domain.coupon.query.usecase.message.IssuedCouponFilter
+
+interface FindCouponUseCase {
+
+    /**
+     * 쿠폰 발급 이력 조회
+     * 쿠폰을 기준으로 발급 이력을 조회한다.
+     */
+    fun findAllCouponHistory(filter: IssuedCouponFilter): IssuedCoupon
 }

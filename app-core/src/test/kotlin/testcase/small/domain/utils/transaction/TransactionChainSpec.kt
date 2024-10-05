381c5717c5f3b669c52e376d2619c4d3e9dfbc40
Picbel <picbel.gon@gmail.com>
2024-10-05 13:55:19 +0900

    [feat] TransactionChain 테스트 작성중

--- app-core/src/test/kotlin/testcase/small/domain/utils/transaction/TransactionChainSpec.kt
+++ app-core/src/test/kotlin/testcase/small/domain/utils/transaction/TransactionChainSpec.kt
@@ -1,12 +1,24 @@
 package testcase.small.domain.utils.transaction
 
+@Suppress("NonAsciiCharacters") // 테스트 코드의 가독성을 위해 함수명과 클레스에 한글을 사용합니다.
 internal class TransactionChainSpec {
 
-    fun ``() {
+
+    fun `TransactionChain의 모든 트랜잭션이 성공합니다`() {
 
     }
 
-    fun ``() {
+    inner class `TransactionChain의 트랜잭션이 실패합니다` {
+        fun `마지막 트랜잭션이 실패하여 전부 롤백됩니다`() {
 
+
+
+        }
+
+        fun ``() {
+
+        }
     }
+
+
 }
